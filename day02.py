ebb452f0ee8da87d5ef9f292e3acf0f91b3f38f6
Saptarshi Paul <149397021+Sapta-Dev27@users.noreply.github.com>
2024-07-07 02:07:11 +0530

    Add files via upload

---
+++ day02.py
@@ -0,0 +1,36 @@
+#escape sequence in python 
+
+print("Hello , I am Saptarshi Paul \nA passionate frontend developer from KOLKATA,INDIA");
+# is used for commenting in code and """" code """" used for multi line code comments 
+# \n is used for moving on to a new line 
+
+""" data types in pythons and variables in python """
+
+a = 10;
+b = 20;
+c = "Saptarshi";
+d= True ;
+print (a );
+print(b);
+print(c);
+print(d);
+
+a1 = a + b;
+print(a1);
+print("The  type of a is : ", type(a));
+print("The  type of b is : " ,type(b));
+print("The type of c is : " , type(c));
+print("The tyepe of d is : " , type(d));
+print("The type of a1 is : ", type(a1));
+
+
+#lists , tuples and dictionaries 
+#lists contains a set of data of different items of  data types and they are mutable in nature
+list1 = [ 1 ,2 , [2.2 , 2.4] , ["Saptarshi " ,"Anuska"] ];
+print(list1);
+#tuples are also of the same kind but they are immutable in nature 
+tuple1 = (1 , 2 , 3 , 4 , 5 , 6);
+print(tuple1);
+#dictionaries are of the same kind but they are mutable in nature and they are used to store
+dict1 = { "name" : "Saptarshi" , "age" : 20 ,  "college" : " Techno Main Salt Lake"};
+print(dict1);
