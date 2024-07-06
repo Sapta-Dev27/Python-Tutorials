e577c6292380e27bde9d31d6b49a0b986661d08d
Saptarshi Paul <149397021+Sapta-Dev27@users.noreply.github.com>
2024-07-07 02:07:29 +0530

    Add files via upload

---
+++ exercise01.py
@@ -0,0 +1,29 @@
+#exercise 1 : To make a calculator that performs all the functions + , - , * , / . %
+
+a = 40; #taking input of num1
+b = 30; #taking input of num2
+
+#addition of two numbers
+add = a+b ;
+print("The addition of " , a  , " and " , b , "is" , add);
+print("The type of add is :" , type(add));
+
+#subtraction of two numbers 
+sub = a- b;
+print("The subtraction of " , a , "and " , b , "is " , sub );
+print("The type of sub is :" , type(sub));
+
+#product of the two numbers 
+mult = a*b;
+print("The product of " , a , "and" , b , "is " , mult);
+print("The type of mult is : " , type(mult));
+
+#division of two numbers 
+div = a/b;
+print("The division of " , a , "and" , b , "is " , div);
+print("The type of div is : " , type(div));
+
+#modulus of two numbers 
+mod = a%b;
+print("The modulus of" , a , "and" , b , "is " , mod);
+print("The type of mod is :" , type(mod));
